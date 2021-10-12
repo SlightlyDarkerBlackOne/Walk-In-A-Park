b8c8a80eab62c064512895789662213ae956772f
SlightlyDarkerBlackOne <42848293+SlightlyDarkerBlackOne@users.noreply.github.com>
2021-10-12 18:09:54 +0200

    Dialogue Sign
    Added Dialogue UI with sign example
    Added animation for opening and closing Dialogue UI

---
+++ Assets/Scripts/ShowSignText.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSignText : MonoBehaviour
+{
+    public GameObject text;
+    private bool alreadyShown = false;
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            if(!alreadyShown)
+                ShowText();   
+        }
+    }
+
+    private void ShowText() {
+        text.SetActive(true);
+        text.GetComponent<Animator>().SetTrigger("show");
+        alreadyShown = true;
+    }
+}
