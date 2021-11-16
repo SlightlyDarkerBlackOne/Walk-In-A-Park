573d0ba503170c6a2e81b4ac5a59396d3a377016
SlightlyDarkerBlackOne <42848293+SlightlyDarkerBlackOne@users.noreply.github.com>
2021-11-16 04:37:04 +0100

    Detecting Scent
    Added Scent script to PlayerDog for task1
    Added scent particle for Vlado
    Removed PeeIcon script

---
+++ Assets/Scent.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scent : MonoBehaviour{
+    [SerializeField]
+    private GameObject scentParticle;
+    private GameObject scent;
+    public static bool ClickedScentIcon = false;
+    private GameObject playerHuman;
+    private bool scenting;
+    public LayerMask detectLayer;
+    public static bool ballFound = false;
+    private Collider2D ball;
+    public GameObject toDoListPanel;
+    private bool checkedVladoScent = false;
+
+    void Start() {
+        playerHuman = GameObject.FindWithTag("Human");
+        detectLayer = LayerMask.GetMask("Item");
+        toDoListPanel = GameObject.Find("ToDo List Panel");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!scenting && (Input.GetKeyDown(KeyCode.G) || ClickedScentIcon)) {
+            checkedVladoScent = true;
+            StartCoroutine("ScentCloud");
+            if (ClickedScentIcon) ClickedScentIcon = false;
+        }
+        if (scent) scent.transform.position = playerHuman.transform.position;
+
+        ball = Physics2D.OverlapCircle(transform.position, 5f, detectLayer);
+        if (ball != null && ball.name == "Vlado Ball" && checkedVladoScent) {
+            ballFound = true;
+            toDoListPanel.GetComponent<TaskManager>().Task1();
+            if (ballFound) Debug.Log("Vlado's ball found");
+            if (!scenting) StartCoroutine("ScentCloud");
+            if (scent) scent.transform.position = ball.transform.position;
+        }
+    }
+
+    private IEnumerator ScentCloud() {
+        scenting = true;
+        scent = Instantiate(scentParticle, Vector2.zero, Quaternion.Euler(0, 0, -118f)) as GameObject;
+        if (!ballFound) scent.transform.position = playerHuman.transform.position;
+        else if (ball != null) scent.transform.position = ball.transform.position;
+        yield return new WaitForSeconds(3);
+        Destroy(scent);
+        scenting = false;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using CodeMonkey.Utils;
 
 public class UIManager : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class UIManager : MonoBehaviour
     private Sprite dogImage;
     public TextMeshProUGUI pickupIndicatiorText;
     public TextMeshProUGUI clickClackIndicatiorText;
+
+    private Button_UI[] button = new Button_UI[2];
 
     #region Singleton
     public static UIManager Instance { get; private set; }
@@ -28,9 +31,28 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetIcons();
         pickupIndicatiorText.gameObject.SetActive(false);
         pickupIndicatiorTextOriginal = pickupIndicatiorText.text;
         dogImage = PlayerController2D.Instance.transform.Find("Animation").GetComponent<SpriteRenderer>().sprite;
+    }
+    void Update() {
+        ManageIcons();
+    }
+
+    void GetIcons() {
+        button[0] = transform.Find("PeeButton").GetComponent<Button_UI>(); //pee icon
+        button[1] = transform.Find("ScentButton").GetComponent<Button_UI>(); //scent icon
+    }
+
+    void ManageIcons() {
+        button[0].GetComponent<Button_UI>().ClickFunc = () => {
+            Pissing.ClickedPeeIcon = true;
+        };
+        button[1].GetComponent<Button_UI>().ClickFunc = () => {
+            Scent.ClickedScentIcon = true;
+        };
+
     }
 
     public void ShowPickupIndicatorText(string name) {
