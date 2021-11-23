48cebb0d7f2c12361d47de534e8d6661546a9232
NevaKis <91982935+NevaKis@users.noreply.github.com>
2021-11-23 21:26:31 +0100

    Park Design
    Put all trees and park objects

---
+++ Assets/Scripts/TaskManager 2.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskManager : MonoBehaviour
+{
+    //task 2 (territory)
+    Dictionary<string,bool> areaDict = new Dictionary<string,bool>();
+    public List<GameObject> peeList = new List<GameObject>();
+    private List<GameObject> toDelete = new List<GameObject>();
+    private Animator anim;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        areaDict.Add("Playground", false);
+        areaDict.Add("Area1", false);
+        areaDict.Add("Area2", false);
+        areaDict.Add("Area3", false);
+        anim = GetComponent<Animator>();        
+    }
+    public bool Task1() {
+        if (Scent.ballFound) {
+            CheckTaskOnList(1);
+            return true;
+        }
+        return false;
+    }
+    public bool Task2()
+    {
+        foreach (var pee in peeList)
+        {
+            //area layer
+            Collider2D area = Physics2D.OverlapCircle(pee.transform.position, 0.1f);
+
+            if (area != null)
+            {
+                Debug.Log(area.name);
+                areaDict[area.name] = true;                             
+            }
+
+            toDelete.Add(pee);
+        }
+        
+        foreach (GameObject pee in toDelete)
+        {
+            peeList.Remove(pee);
+        }
+
+        foreach (var area in areaDict)
+        {
+            if (area.Value == false) return false;
+        }
+
+        CheckTaskOnList(2);
+        return true;
+    }
+    //changes the toggle's Normal colour to the new colour
+    void CheckTaskOnList(int taskNumber) {
+        GameObject task = gameObject.transform.GetChild(1).GetChild(taskNumber - 1).gameObject;
+        task.GetComponent<Toggle>().isOn = true;
+        ColorBlock cb = task.GetComponent<Toggle>().colors;
+        cb.normalColor = Color.black;
+        task.GetComponent<Toggle>().colors = cb;
+        anim.SetBool("isShowing", true);
+    }
+}
---
+++ Assets/Scripts/TransparentWhenPlayerBehind 2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentWhenPlayerBehind : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float alpha;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            if(collision.transform.Find("Animation").GetComponent<SpriteRenderer>()
+            .sortingOrder < this.GetComponent<SpriteRenderer>().sortingOrder) {
+                ChangeAlpha(alpha);
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            if (collision.transform.Find("Animation").GetComponent<SpriteRenderer>()
+            .sortingOrder < this.GetComponent<SpriteRenderer>().sortingOrder) {
+                ChangeAlpha(alpha);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        ChangeAlpha(1f);
+    }
+
+    public void ChangeAlpha(float alphaValue) {
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, alphaValue);
+    }
+}
