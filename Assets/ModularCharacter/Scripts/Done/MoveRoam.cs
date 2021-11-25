059374c62ffa23f13b72681b5d93e8379d3fd3f7
SlightlyDarkerBlackOne <42848293+SlightlyDarkerBlackOne@users.noreply.github.com>
2021-11-25 20:45:51 +0100

    Polishing
    Added dialog background box
    Readjusted dialogue text to not go over the box
    Added animated arrow for clicking dialogue
    Added npcmoveSpeed min and max
    Scaled MilicaScent to be bigger 2x
    Readjusted global light intensity so it starts as day
    Changed grass color to green

--- Assets/ModularCharacter/Scripts/Done/MoveRoam.cs
+++ Assets/ModularCharacter/Scripts/Done/MoveRoam.cs
@@ -15,29 +15,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using CodeMonkey.Utils;
+using UnityEngine.AI;
 
 public class MoveRoam : MonoBehaviour {
 
-    public float movementRangeX = 3f;
-    public float movementRangeY = 3f;
+    public float npcMoveSpeedMin = 0.8f;
+    public float npcMoveSpeedMax = 3f;
 
     public Transform targetToMoveAwayFrom;
 
     private Vector3 startPosition;
     private Vector3 targetMovePosition;
     private AgentRoamer agent;
+    private NavMeshAgent navmeshAgent;
     private void Awake() {
         startPosition = transform.position;
         agent = GetComponent<AgentRoamer>();
     }
 
     private void Start() {
+        navmeshAgent = GetComponent<NavMeshAgent>();
         SetRandomMovePosition();
     }
 
     private void SetRandomMovePosition() {
         //targetMovePosition = startPosition + UtilsClass.GetRandomDir() * Random.Range(movementRangeX, movementRangeY);
         targetMovePosition = GetRandomWaypoint();
+        navmeshAgent.speed = Random.Range(npcMoveSpeedMin, npcMoveSpeedMax);
     }
 
     private void Update() {
